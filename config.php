87f31e407db6e463ddf058ba738409e82902aa04
Paweł Dymon <dymonpawel@gmail.com>
2022-07-07 19:41:26 +0200

    add style in index.php

--- config.php
+++ config.php
@@ -1,11 +1,11 @@
 <?php
 
-// $host = 'localhost';
-// $user = 'ljaphnfptv_ljaphnfptv';
-// $password = 'qsML2V5LVWu@';
-// $database = 'ljaphnfptv_superTapety';
-
 $host = 'localhost';
-$user = 'root';
-$password = '';
-$database = 'super_tapety';
+$user = 'ljaphnfptv_ljaphnfptv';
+$password = 'qsML2V5LVWu@';
+$database = 'ljaphnfptv_superTapety';
+
+// $host = 'localhost';
+// $user = 'root';
+// $password = '';
+// $database = 'super_tapety';
